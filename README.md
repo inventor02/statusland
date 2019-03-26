f071245dc9a7d34dc2c445c1a0a83dd1b7fc4407
George Peppard <iaminventor02@gmail.com>
2019-03-26 18:29:15 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# statusland
+
+Simple landing page for servers displaying system status including platform, uptime and RAM usage.
+
+Licensed under GPL-3.0.
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const si = require('systeminformation');
+
+const config = require('./config.json');
+
+const app = express();
+const port = config.port;
+
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultView: 'default',
+    layoutsDir: '/views/pages',
+    partialsDir: '/views/partials'
+}));
+
+app.get('/', (req, res) => {
+    let info = {};
+
+    si.mem()
+        .then(data => {
+            info['memory'] = (data.used / 1000000000).toFixed(1) + 'GB / ' + (data.total / 1000000000).toFixed(1) + 'GB';
+
+            si.osInfo()
+                .then(osData => {
+                    info['hostname'] = osData.hostname;
+                    info['platform'] = osData.platform;
+
+                    si.currentLoad()
+                        .then(loadData => {
+                            info['load'] = loadData.currentload.toFixed(1) + '%';
+
+                            info['uptime'] = (si.time().uptime / 3600).toFixed('1') + 'HR';
+
+                            res.render('index', {
+                                info: info
+                            });
+                        })
+                        .catch(error => console.error(error));
+                })
+                .catch(error => console.error(error));
+        })
+        .catch(error => console.error(error));
+});
+
+app.listen(port);
